0e9dd39d2d327b3370f653cfa6a82a6f4a53139e
kyubin-moreh <kyubin.kim@moreh.io>
2023-06-05 18:18:40 +0900

    deleted redundant codes

--- util/dali_dataloader.py
+++ util/dali_dataloader.py
@@ -28,22 +28,6 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 
-def fast_collate(batch, memory_format):
-    """Based on fast_collate from the APEX example
-       https://github.com/NVIDIA/apex/blob/5b5d41034b506591a316c308c3d2cd14d5187e23/examples/imagenet/main_amp.py#L265
-    """
-    imgs = [img[0] for img in batch]
-    targets = torch.tensor([target[1] for target in batch], dtype=torch.int64)
-    w = imgs[0].size[0]
-    h = imgs[0].size[1]
-    tensor = torch.zeros( (len(imgs), 3, h, w), dtype=torch.uint8).contiguous(memory_format=memory_format)
-    for i, img in enumerate(imgs):
-        nump_array = np.asarray(img, dtype=np.uint8)
-        if(nump_array.ndim < 3):
-            nump_array = np.expand_dims(nump_array, axis=-1)
-        nump_array = np.rollaxis(nump_array, 2)
-        tensor[i] += torch.from_numpy(nump_array)
-    return tensor, targets
 
 def get_dali_dataloader(
     batch_size,
